16ec7d52014ec189959d6efc4b1364e40390627c
Pak_Man926 <davidkamau926@gmail.com>
2025-08-17 00:16:57 +0300

    AppBar setup

--- mealmuse_flutter/lib/screens/homepage.dart
+++ mealmuse_flutter/lib/screens/homepage.dart
@@ -1,10 +1,30 @@
 import "package:flutter/material.dart";
+import "package:google_fonts/google_fonts.dart";
 
 
 class Homepage extends StatelessWidget
 {
   const Homepage({super.key});
 
-  return Scaffold();
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Meal Muse",
+          style: GoogleFonts.poppins(
+            fontSize: 24,
+            fontWeight: FontWeight.w700,
+          )),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.tune, size: 15),
+          ),
+        ],
+      ),
+    );
+  }
 
 }
