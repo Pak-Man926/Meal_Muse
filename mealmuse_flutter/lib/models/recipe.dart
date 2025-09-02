3fd02a854f0291a270eb3a2c7e3dc5ccdbd46b18
Pak_Man926 <davidkamau926@gmail.com>
2025-09-02 17:08:09 +0300

    Addition of image to the recipe model

--- mealmuse_flutter/lib/models/recipe.dart
+++ mealmuse_flutter/lib/models/recipe.dart
@@ -1,11 +1,13 @@
 class Recipe 
 {
+  final String imageUrl;
   final String name;
   final String description;
   final List<String> ingredients;
   final List<String> procedure;
 
   Recipe({
+    required this.imageUrl,
     required this.name,
     required this.description,
     required this.ingredients,
