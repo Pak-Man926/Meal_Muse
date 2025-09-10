8f0d176a1a85b100f4e2d11cbe6e81c598cfaaf8
Pak_Man926 <davidkamau926@gmail.com>
2025-09-10 15:08:30 +0300

    Defination of the screens and bottom  navigation bar

--- mealmuse_flutter/lib/app.dart
+++ mealmuse_flutter/lib/app.dart
@@ -1,15 +1,41 @@
 import "package:flutter/material.dart";
 import "package:get/get.dart";
+import "package:mealmuse_flutter/views/homepage/homepage.dart";
+import "package:mealmuse_flutter/views/savedpage/savedpage.dart";
+import "package:mealmuse_flutter/views/schedulepage/schedulepage.dart";
+import "package:mealmuse_flutter/views/searchpage/searchpage.dart";
 
 import "controllers/theme_controller.dart";
 import "routes.dart";
 import "themes/theme.dart";
 import "widgets/tune_icon_button_widget.dart";
+import "widgets/bottom_navigation_bar.dart";
 
-class AppPage extends StatelessWidget
+class AppPage extends StatefulWidget
 {
+  @override
+  State<AppPage> createState() => _AppPageState();
+}
+
+class _AppPageState extends State<AppPage> {
   //const AppPage({super.key});
   final ThemeController themeController = Get.put(ThemeController());
+
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index)
+  {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List<Widget> _pages = [
+    HomeScreen(),
+    SearchScreen(),
+    ScheduleScreen(),
+    SavedScreen()
+  ];
 
   @override
   Widget build(BuildContext context)
@@ -24,12 +50,21 @@ class AppPage extends StatelessWidget
          TuneIconButtonWidget(
           onPressed: ()
           {
-            
+
           },
           iconSize: 30,
           color: Colors.black,
          ), 
         ],
+      ),
+      body: SafeArea(
+        top: false,
+        bottom: true,
+        child: _pages[_selectedIndex],
+      ),
+      bottomNavigationBar:  AppBottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
     );
   }
