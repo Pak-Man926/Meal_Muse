85a392cbfc6c8bec455c8ef297b1fa9485a45577
Pak_Man926 <davidkamau926@gmail.com>
2025-09-15 15:00:17 +0300

    Carousel slider improvement

--- mealmuse_flutter/lib/widgets/carousel_slider_widget.dart
+++ mealmuse_flutter/lib/widgets/carousel_slider_widget.dart
@@ -1,21 +1,15 @@
 import "package:flutter/material.dart";
 import "package:carousel_slider/carousel_slider.dart";
+import "package:mealmuse_flutter/models/carousel_items.dart";
 
 class CarouselSliderWidget extends StatelessWidget
 {
+    final List<CarouselItems> items;
+
     CarouselSliderWidget({
     super.key,
-    //required this.imageUrls,
+    required this.items,
     });
-
-   final List<String> imageUrls = [
-    'https://picsum.photos/300/450?1',
-    'https://picsum.photos/300/450?2',
-    'https://picsum.photos/300/450?3',
-    'https://picsum.photos/300/450?4',
-    'https://picsum.photos/300/450?5',
-    'https://picsum.photos/300/450?6',
-  ];
 
   @override
   Widget build(BuildContext context)
@@ -30,29 +24,47 @@ class CarouselSliderWidget extends StatelessWidget
           autoPlayInterval: Duration(seconds:3),
           scrollDirection: Axis.horizontal,
           ),
-        items: imageUrls.map((url)
+        items: items.map((item)
         {
           return Builder(
             builder: (BuildContext context)
             {
-              return Container(
-                width: 300,
-                height: 450,
-                margin: const EdgeInsets.symmetric(horizontal:5.0),
-                decoration: BoxDecoration(
-                  color: Colors.grey,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Image.network(
-                  url,
-                  fit: BoxFit.cover,
-                  errorBuilder: (context, error, stackTrace)
-                  {
-                    return const Center(
-                      child: Icon(Icons.error),
-                    );
-                  },
-                ), 
+              return Column(
+                children: <Widget>[
+                  Container(
+                    width: 300,
+                    height: 350,
+                    margin: const EdgeInsets.symmetric(horizontal: 10.0),
+                    decoration: BoxDecoration(
+                      color: Colors.grey, 
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10.0),
+                      child: Image.network(
+                        item.imageUrls,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          item.title,
+                          style: Theme.of(context).textTheme.headlineMedium,
+                        ),
+                        const SizedBox(height: 4.0),
+                        Text(
+                          item.description,
+                          style: Theme.of(context).textTheme.bodyMedium,
+                        ),
+                      ]
+                    )
+                  ),
+                ]
               );
             }
           );
