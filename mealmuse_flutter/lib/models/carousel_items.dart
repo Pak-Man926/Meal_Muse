61852db083cb7e58083916194a6996073f9ba084
Pak_Man926 <davidkamau926@gmail.com>
2025-09-15 14:59:57 +0300

    Carousel items model

---
+++ mealmuse_flutter/lib/models/carousel_items.dart
@@ -0,0 +1,12 @@
+class CarouselItems 
+{
+  final String imageUrls;
+  final String title;
+  final String description;
+
+  CarouselItems({
+    required this.imageUrls,
+    required this.title,
+    required this.description,
+  });
+}
