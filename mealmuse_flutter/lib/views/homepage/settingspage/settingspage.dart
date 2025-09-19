94aca80de853ce1176f81b3362a76802d0081a4f
Pak_Man926 <davidkamau926@gmail.com>
2025-09-19 17:15:04 +0300

    Settings page design and app callback configuration

--- mealmuse_flutter/lib/views/homepage/settingspage/settingspage.dart
+++ mealmuse_flutter/lib/views/homepage/settingspage/settingspage.dart
@@ -1,1 +1,38 @@
-import
+import "package:flutter/material.dart";
+import "package:get/get.dart";
+
+class SettingsPage extends StatelessWidget
+{
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => Get.back,
+          icon: Icon(Icons.arrow_back_rounded),
+          iconSize: 24,
+          color: Theme.of(context).colorScheme.onSurface,
+        ),
+        centerTitle: true,
+        title: Text("Settings",
+          style: Theme.of(context).textTheme.displayLarge),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Preferences",
+              style: Theme.of(context).textTheme.displayMedium
+            ),
+            SizedBox(height: 5),
+            
+          ],),
+      )
+    );
+  }
+}
