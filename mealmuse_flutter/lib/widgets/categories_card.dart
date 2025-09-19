7092328b3be3ae384c07061f6d383d34ba6a05a6
Pak_Man926 <davidkamau926@gmail.com>
2025-09-19 15:46:01 +0300

    Categories card design completion

--- mealmuse_flutter/lib/widgets/categories_card.dart
+++ mealmuse_flutter/lib/widgets/categories_card.dart
@@ -3,27 +3,29 @@ import "package:flutter/material.dart";
 class CategoriesCard extends StatelessWidget {
   final Icon icon;
   final String title;
-  final Function ontap;
 
   const CategoriesCard({
     super.key,
     required this.icon,
     required this.title,
-    required this.ontap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Card.outlined(
-      child: ListTile(
-        leading: icon,
-        title: Text(
-          title,
-          style: Theme.of(context).textTheme.labelMedium,
-          textDirection: TextDirection.ltr,
+    return SizedBox(
+      height: 50,
+      width: 100,
+      child: OutlinedButton(
+        onPressed: null,
+        child: Row(
+          children: [
+            icon,
+            SizedBox(width: 5),
+            Text(title, style: Theme.of(context).textTheme.labelLarge),
+          ],
         ),
-        onTap: () => ontap,
-      ),
+        
+      )
     );
   }
 }
