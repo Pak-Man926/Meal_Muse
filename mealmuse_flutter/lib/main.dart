8a82a2b1534aa6df25bfdfb2d63707093cad53e4
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2026-02-23 15:11:25 +0300

    Removal of serverpod dependancies

--- mealmuse_flutter/lib/main.dart
+++ mealmuse_flutter/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:mealmuse_client/mealmuse_client.dart';
 import 'package:flutter/material.dart';
 //import 'package:serverpod_flutter/serverpod_flutter.dart';
 //import "package:google_fonts/google_fonts.dart";
@@ -7,29 +6,11 @@ import "package:get/get.dart";
 import "routes.dart";
 import "themes/theme.dart";
 
-/// Sets up a global client object that can be used to talk to the server from
-/// anywhere in our app. The client is generated from your server code
-/// and is set up to connect to a Serverpod running on a local server on
-/// the default port. You will need to modify this to connect to staging or
-/// production servers.
-/// In a larger app, you may want to use the dependency injection of your choice
-/// instead of using a global client object. This is just a simple example.
-late final Client client;
 
 late String serverUrl;
 
 void main() {
-  // When you are running the app on a physical device, you need to set the
-  // server URL to the IP address of your computer. You can find the IP
-  // address by running `ipconfig` on Windows or `ifconfig` on Mac/Linux.
-  // You can set the variable when running or building your app like this:
-  // E.g. `flutter run --dart-define=SERVER_URL=https://api.example.com/`
-  // const serverUrlFromEnv = String.fromEnvironment('SERVER_URL');
-  // final serverUrl =
-  //     serverUrlFromEnv.isEmpty ? 'http://$localhost:8080/' : serverUrlFromEnv;
 
-  // client = Client(serverUrl)
-  //   ..connectivityMonitor = FlutterConnectivityMonitor();
 
   runApp(const MyApp());
 }
