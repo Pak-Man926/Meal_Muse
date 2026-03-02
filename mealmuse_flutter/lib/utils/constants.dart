12a1b1ae44122d34178fede5e6fcc09092cfc941
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2026-03-02 22:05:54 +0300

    Addition of constants

---
+++ mealmuse_flutter/lib/utils/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const String appName = "Meal Muse";
+const String apiBaseUrl = "https://www.themealdb.com/api/json/v1/1/";
+
+const tinySpaceSize = SizedBox(height: 5);
+const smallSpaceSize = SizedBox(height: 10);
+const minSpaceSize = SizedBox(height: 15);
+const mediumSpaceSize = SizedBox(height: 20);
