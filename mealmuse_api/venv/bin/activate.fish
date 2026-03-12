476937e57db5a0c9760ff3671047af9e98765060
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2026-03-12 15:06:53 +0300

    Venv files

--- mealmuse_api/venv/bin/activate.fish
+++ mealmuse_api/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/pak_man/Desktop/recipe_api/venv
+set -gx VIRTUAL_ENV /home/pak_man/Desktop/Meal_Muse/mealmuse_api/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
