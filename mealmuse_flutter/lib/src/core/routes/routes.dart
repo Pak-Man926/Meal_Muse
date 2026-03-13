dd973bdb9cb2e352f5165a8d58803f1778d4d7ca
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2026-03-13 16:43:51 +0300

    Created the settings page plus its route definition

--- mealmuse_flutter/lib/src/core/routes/routes.dart
+++ mealmuse_flutter/lib/src/core/routes/routes.dart
@@ -1,12 +1,17 @@
 import "package:go_router/go_router.dart";
-import "package:meal_muse/src/app.dart";
+import "package:meal_muse/src/features/homeview/presentation/home_view.dart";
+import "package:meal_muse/src/features/settingspage/presentation/settings_page.dart";
 
 final router = GoRouter(
   initialLocation: "/",
   routes: [
     GoRoute(
       path: "/",
-      builder: (context, state) => const MyApp(),
+      builder: (context, state) => HomeView(),
+    ),
+    GoRoute(
+      path: "/settings",
+      builder: (context, state) => SettingsPageView(),
     ),
   ]
 );
