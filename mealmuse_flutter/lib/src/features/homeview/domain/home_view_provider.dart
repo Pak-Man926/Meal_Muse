0e66721774c17b9c3ab8de0715f0b119355a2147
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2026-03-16 16:36:10 +0300

    Renaming from controller to provider to ensure data consistency

--- mealmuse_flutter/lib/src/features/homeview/domain/home_view_provider.dart
+++ mealmuse_flutter/lib/src/features/homeview/domain/home_view_provider.dart
@@ -1,10 +1,4 @@
-import "package:flutter/material.dart";
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_riverpod/legacy.dart';
-import '../../homepage/presentation/home_page.dart';
-import '../../savedpage/presentation/saved_view.dart';
-import '../../schedulepage/presentation/schedule_view.dart';
-import '../../searchpage/presentation/search_view.dart';
 
 class BottomNavNotifier extends Notifier<int> {
   @override
