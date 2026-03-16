d594de23939a04e6aae306fbc931f67fa8206a16
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2026-03-16 16:37:06 +0300

    Search page setup with few changes remaining for completion

--- mealmuse_flutter/lib/src/features/searchpage/presentation/search_view.dart
+++ mealmuse_flutter/lib/src/features/searchpage/presentation/search_view.dart
@@ -1,12 +1,61 @@
 import "package:flutter/material.dart";
+import "package:meal_muse/src/core/constants/constants.dart";
+import "package:meal_muse/src/core/themes/text_styles.dart";
+import "package:meal_muse/src/features/searchpage/data/container_widget.dart";
 
-class SearchPageView extends StatelessWidget
-{
+class SearchPageView extends StatelessWidget {
   const SearchPageView({super.key});
 
   @override
-  Widget build(BuildContext context)
-  {
-    return const Scaffold();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Search", style: AppTextStyles.headingsText),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          mainAxisAlignment: .start,
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Search for recipes",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      prefixIcon: Icon(Icons.search),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            mediumSpaceSize,
+            Wrap(
+              spacing: 10,
+              runSpacing: 10,
+              children: [
+                ContainerWidget(label: "Quick Recipes"),
+                ContainerWidget(label: "Healthy"),
+                ContainerWidget(label: "Vegan"),
+                ContainerWidget(label: "Gluten-free"),
+                ContainerWidget(label: "Italian"),
+                ContainerWidget(label: "Under 30 minutes"),
+                ContainerWidget(label: "Breakfast"),
+              ],
+            ),
+            mediumSpaceSize,
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text("Results", style: AppTextStyles.subHeadingsText),
+            ),
+            mediumSpaceSize,
+          ],
+        ),
+      ),
+    );
   }
 }
