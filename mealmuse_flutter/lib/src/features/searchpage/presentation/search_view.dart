4bc1e4013379bf3d7a7f998eaaeb5f4f9f15f86e
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2026-03-17 15:17:47 +0300

    Search view UI design completion.

--- mealmuse_flutter/lib/src/features/searchpage/presentation/search_view.dart
+++ mealmuse_flutter/lib/src/features/searchpage/presentation/search_view.dart
@@ -3,6 +3,8 @@ import "package:meal_muse/src/core/constants/constants.dart";
 import "package:meal_muse/src/core/themes/text_styles.dart";
 import "package:meal_muse/src/features/searchpage/data/container_widget.dart";
 
+import "../../../widgets/items_widget.dart";
+
 class SearchPageView extends StatelessWidget {
   const SearchPageView({super.key});
 
@@ -53,6 +55,41 @@ class SearchPageView extends StatelessWidget {
               child: Text("Results", style: AppTextStyles.subHeadingsText),
             ),
             mediumSpaceSize,
+            ItemsWidget(
+              image: Image.asset("assets/Chicken-stir-fry-V1.jpg"),
+              heading: "Chicken Stir Fry",
+              subHeading: "30 mins | 4 servings",
+            ),
+            mediumSpaceSize,
+            ItemsWidget(
+              image: Image.asset("assets/Fluffy-Pancakes-Featured.jpg"),
+              heading: "Fluffy Pancakes",
+              subHeading: "20 mins | 4 servings",
+            ),
+            mediumSpaceSize,
+            ItemsWidget(
+              image: Image.asset("assets/Pasta-Carbonara-Recipe-1.jpg"),
+              heading: "Pasta Carbonara",
+              subHeading: "25 mins | 4 servings",
+            ),
+            mediumSpaceSize,
+            ItemsWidget(
+              image: Image.asset("assets/vegetable-curry-recipe.jpg"),
+              heading: "Vegetable Curry",
+              subHeading: "30 mins | 4 servings",
+            ),
+            mediumSpaceSize,
+            ItemsWidget(
+              image: Image.asset("assets/201005-r-xl-grilled-chicken-tacos-2000-63b2b629eace4d71a7ee63529e252c38.jpg"),
+              heading: "Chicken Tacos",
+              subHeading: "48 mins | 4 servings",
+            ),
+            mediumSpaceSize,
+            ItemsWidget(
+              image: Image.asset("assets/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2007__04__honey-glazed-roast-chicken-horiz-a-1800-2057270028084ff2bdb54fcb0f2d3227.jpg"),
+              heading: "Honey Glazed Roast Chicken",
+              subHeading: "1 hour | 4 servings",
+            ),
           ],
         ),
       ),
