1c4b3bb5f9d3aeeadb6d6c9c96a4ee38d7c4bd08
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2026-03-19 17:21:57 +0300

    Schedule page changes: Date display for dates of the month

--- mealmuse_flutter/lib/src/features/schedulepage/presentation/schedule_view.dart
+++ mealmuse_flutter/lib/src/features/schedulepage/presentation/schedule_view.dart
@@ -1,9 +1,11 @@
 import "package:flutter/material.dart";
 import "package:meal_muse/src/core/constants/constants.dart";
 import "package:meal_muse/src/features/schedulepage/data/date_container_widget.dart";
+import "package:meal_muse/src/features/searchpage/data/container_widget.dart";
 
 import "../../../core/themes/text_styles.dart";
 import "../../../widgets/items_widget.dart";
+import 'package:intl/intl.dart';
 
 class SchedulePageView extends StatelessWidget {
   const SchedulePageView({super.key});
@@ -18,69 +20,79 @@ class SchedulePageView extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(10.0),
         child: Column(
-          mainAxisAlignment: .start,
+          crossAxisAlignment: .start,
           children: [
-            // Align(
-            //   alignment: Alignment.topLeft,
-            //   child: Text("This Week", style: AppTextStyles.subHeadingsText),
-            // ),
-            // mediumSpaceSize,
-            // ItemsWidget(
-            //   image: Image.asset("assets/Chicken-stir-fry-V1.jpg"),
-            //   heading: "Chicken Stir Fry",
-            //   subHeading: "30 mins | 4 servings",
-            // ),
-            // mediumSpaceSize,
-            // ItemsWidget(
-            //   image: Image.asset("assets/Fluffy-Pancakes-Featured.jpg"),
-            //   heading: "Fluffy Pancakes",
-            //   subHeading: "20 mins | 4 servings",
-            // ),
-            // mediumSpaceSize,
-            // ItemsWidget(
-            //   image: Image.asset("assets/Pasta-Carbonara-Recipe-1.jpg"),
-            //   heading: "Pasta Carbonara",
-            //   subHeading: "25 mins | 4 servings",
-            // ),
-            // mediumSpaceSize,
-            // ItemsWidget(
-            //   image: Image.asset("assets/vegetable-curry-recipe.jpg"),
-            //   heading: "Vegetable Curry",
-            //   subHeading: "30 mins | 4 servings",
-            // ),
-            // mediumSpaceSize,
-            // ItemsWidget(
-            //   image: Image.asset(
-            //     "assets/201005-r-xl-grilled-chicken-tacos-2000-63b2b629eace4d71a7ee63529e252c38.jpg",
-            //   ),
-            //   heading: "Chicken Tacos",
-            //   subHeading: "48 mins | 4 servings",
-            // ),
-            // mediumSpaceSize,
-            // ItemsWidget(
-            //   image: Image.asset(
-            //     "assets/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2007__04__honey-glazed-roast-chicken-horiz-a-1800-2057270028084ff2bdb54fcb0f2d3227.jpg",
-            //   ),
-            //   heading: "Honey Glazed Roast Chicken",
-            //   subHeading: "1 hour | 4 servings",
-            // ),
-            // mediumSpaceSize,
-            // ItemsWidget(
-            //   image: Image.asset(
-            //     "assets/pan-fried-salmon-featured-new.jpg",
-            //   ),
-            //   heading: "Pan-Fried Salmon",
-            //   subHeading: "20 mins | 4 servings",
-            // ),
-            Text(
-              "${DateTime.now().month}  ${DateTime.now().year}",
-              style: AppTextStyles.bodyText,
-            ),
+            Text("$currentMonth $currentYear", style: AppTextStyles.bodyText),
             mediumSpaceSize,
-            DatePickerWidget(day: "${DateTime.now().weekday - 2}", date: DateTime.now().day - 2),
+            SizedBox(
+              height: 130, // Adjust this to fit your DatePickerWidget
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 7,
+                itemBuilder: (context, index) {
+                  DateTime targetDate = today.add(Duration(days: index));
+                  String dayName = weekdayNames[targetDate.weekday - 1];
+                  return DatePickerWidget(
+                    day: dayName,
+                    date: targetDate.day, // Extracts just the number (e.g., 19)
+                  );
+                },
+              ),
+            ),
+            smallSpaceSize,
+            Row(
+              mainAxisAlignment: .spaceBetween,
+              children: [
+                Text(
+                  "$currentDay, $currentMonth ${today.day}",
+                  style: AppTextStyles.subHeadingsText,
+                ),
+                // Container(
+                //   height: 30,
+                //   decoration: BoxDecoration(
+                //     border: Border.all(),
+                //     borderRadius: BorderRadius.circular(20),
+                //     shape: BoxShape.rectangle,
+                //      ),
+                //      child: Text("3 Meals Planned", style: AppTextStyles.bodyText),
+                // ),
+                ContainerWidget.extended(label: "3 Meals Planned"),
+              ],
+            ),
           ],
         ),
       ),
     );
   }
 }
+
+const List<String> monthNames = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+const List<String> weekdayNames = [
+  'Mon',
+  'Tue',
+  'Wed',
+  'Thu',
+  'Fri',
+  'Sat',
+  'Sun',
+];
+
+DateTime today = DateTime.now();
+
+String currentMonth = monthNames[today.month - 1];
+String currentYear = today.year.toString();
+String currentDay = DateFormat("EEE").format(today);
