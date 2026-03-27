3fc849167e42920c29379326e2ef1a254949f2f8
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2026-03-27 22:37:48 +0300

    Setting up the item view structure

---
+++ mealmuse_flutter/lib/src/features/itemview/presentation/item_view.dart
@@ -0,0 +1,12 @@
+import "package:flutter/material.dart";
+
+class ItemView extends StatelessWidget
+{
+  const ItemView({super.key});
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold();
+  }
+}
