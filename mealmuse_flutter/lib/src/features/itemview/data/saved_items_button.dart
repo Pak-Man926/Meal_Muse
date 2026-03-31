8ad48c315e80a10c368a47b524249c2a222de6bd
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2026-03-31 09:48:45 +0300

    Definition of saved/schedule items button

---
+++ mealmuse_flutter/lib/src/features/itemview/data/saved_items_button.dart
@@ -0,0 +1,44 @@
+import "package:flutter/material.dart";
+import "package:meal_muse/src/core/themes/colors.dart";
+import "package:meal_muse/src/core/themes/text_styles.dart";
+
+class ViewItemsButton extends StatelessWidget {
+  final String label;
+  final Function()? onPressed;
+  final Color? color;
+
+  const ViewItemsButton({
+    super.key,
+    required this.label,
+    this.onPressed,
+    this.color = const Color.fromARGB(255, 200, 199, 199),
+  });
+
+  const ViewItemsButton.primary({
+    super.key,
+    required this.label,
+    this.onPressed,
+    this.color = AppColors.primary,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      width: 150,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: TextButton(
+        onPressed: onPressed,
+        child: Expanded(
+          child: Text(
+            label,
+            style: AppTextStyles.bodyText.copyWith(color: Colors.black),
+          ),
+        ),
+      ),
+    );
+  }
+}
