74d619ad23dec50e3ff50236ef3b846e8f0fce4e
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2026-03-31 09:44:05 +0300

    Addition of safe area to the app

--- mealmuse_flutter/lib/src/app.dart
+++ mealmuse_flutter/lib/src/app.dart
@@ -2,19 +2,21 @@ import "package:flutter/material.dart";
 import "package:meal_muse/src/core/routes/routes.dart";
 import "package:meal_muse/src/core/themes/theme.dart";
 
-class MyApp extends StatelessWidget
-{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context)
-  {
-    return MaterialApp.router(
-      routerConfig: router,
-      debugShowCheckedModeBanner: false,
-      theme: lightTheme,
-      darkTheme: darkTheme,
-      themeMode: ThemeMode.system,
+  Widget build(BuildContext context) {
+    return SafeArea(
+      top: false,
+      bottom: true,
+      child: MaterialApp.router(
+        routerConfig: router,
+        debugShowCheckedModeBanner: false,
+        theme: lightTheme,
+        darkTheme: darkTheme,
+        themeMode: ThemeMode.system,
+      ),
     );
   }
 }
