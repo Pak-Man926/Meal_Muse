1e5a5281d126a3891bbb38c14cb4601ef59d95b6
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2026-04-09 17:25:38 +0300

    Search screen results scrollability effect

--- mealmuse_flutter/lib/src/features/search/presentation/search_screen.dart
+++ mealmuse_flutter/lib/src/features/search/presentation/search_screen.dart
@@ -52,44 +52,57 @@ class SearchScreen extends StatelessWidget {
             mediumSpaceSize,
             Text("Results", style: AppTextStyles.sectionHeader),
             mediumSpaceSize,
-            RecipeCardWidget(
-              image: Image.asset("assets/Chicken-stir-fry-V1.jpg"),
-              heading: "Chicken Stir Fry",
-              subHeading: "30 mins | 4 servings",
-            ),
-            mediumSpaceSize,
-            RecipeCardWidget(
-              image: Image.asset("assets/Fluffy-Pancakes-Featured.jpg"),
-              heading: "Fluffy Pancakes",
-              subHeading: "20 mins | 4 servings",
-            ),
-            mediumSpaceSize,
-            RecipeCardWidget(
-              image: Image.asset("assets/Pasta-Carbonara-Recipe-1.jpg"),
-              heading: "Pasta Carbonara",
-              subHeading: "25 mins | 4 servings",
-            ),
-            mediumSpaceSize,
-            RecipeCardWidget(
-              image: Image.asset("assets/vegetable-curry-recipe.jpg"),
-              heading: "Vegetable Curry",
-              subHeading: "30 mins | 4 servings",
-            ),
-            mediumSpaceSize,
-            RecipeCardWidget(
-              image: Image.asset(
-                "assets/201005-r-xl-grilled-chicken-tacos-2000-63b2b629eace4d71a7ee63529e252c38.jpg",
+            Expanded(
+              child: ListView.builder(
+                itemCount: 6,
+                shrinkWrap: true,
+                //physics: NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      RecipeCardWidget(
+                        image: Image.asset("assets/Chicken-stir-fry-V1.jpg"),
+                        heading: "Chicken Stir Fry",
+                        subHeading: "30 mins | 4 servings",
+                      ),
+                      mediumSpaceSize,
+                      RecipeCardWidget(
+                        image: Image.asset("assets/Fluffy-Pancakes-Featured.jpg"),
+                        heading: "Fluffy Pancakes",
+                        subHeading: "20 mins | 4 servings",
+                      ),
+                      mediumSpaceSize,
+                      RecipeCardWidget(
+                        image: Image.asset("assets/Pasta-Carbonara-Recipe-1.jpg"),
+                        heading: "Pasta Carbonara",
+                        subHeading: "25 mins | 4 servings",
+                      ),
+                      mediumSpaceSize,
+                      RecipeCardWidget(
+                        image: Image.asset("assets/vegetable-curry-recipe.jpg"),
+                        heading: "Vegetable Curry",
+                        subHeading: "30 mins | 4 servings",
+                      ),
+                      mediumSpaceSize,
+                      RecipeCardWidget(
+                        image: Image.asset(
+                          "assets/201005-r-xl-grilled-chicken-tacos-2000-63b2b629eace4d71a7ee63529e252c38.jpg",
+                        ),
+                        heading: "Chicken Tacos",
+                        subHeading: "48 mins | 4 servings",
+                      ),
+                      mediumSpaceSize,
+                      RecipeCardWidget(
+                        image: Image.asset(
+                          "assets/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2007__04__honey-glazed-roast-chicken-horiz-a-1800-2057270028084ff2bdb54fcb0f2d3227.jpg",
+                        ),
+                        heading: "Honey Glazed Roast Chicken",
+                        subHeading: "1 hour | 4 servings",
+                      ),
+                    ],
+                  );
+                },
               ),
-              heading: "Chicken Tacos",
-              subHeading: "48 mins | 4 servings",
-            ),
-            mediumSpaceSize,
-            RecipeCardWidget(
-              image: Image.asset(
-                "assets/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2007__04__honey-glazed-roast-chicken-horiz-a-1800-2057270028084ff2bdb54fcb0f2d3227.jpg",
-              ),
-              heading: "Honey Glazed Roast Chicken",
-              subHeading: "1 hour | 4 servings",
             ),
           ],
         ),
