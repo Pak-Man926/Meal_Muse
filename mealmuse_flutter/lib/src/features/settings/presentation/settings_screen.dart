46c6ca821781858f54490ef7d25bda473e4b6655
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2026-04-15 17:44:40 +0300

    Cmpilation of the designed widgets but awating a few changes before completion.

--- mealmuse_flutter/lib/src/features/settings/presentation/settings_screen.dart
+++ mealmuse_flutter/lib/src/features/settings/presentation/settings_screen.dart
@@ -1,6 +1,9 @@
 import "package:flutter/material.dart";
 import "package:meal_muse/src/core/constants/constants.dart";
 import "package:meal_muse/src/core/themes/text_styles.dart";
+import "package:meal_muse/src/features/settings/presentation/widgets/item_checkbox_widget.dart";
+import "package:meal_muse/src/features/settings/presentation/widgets/item_switch_widget.dart";
+import "package:meal_muse/src/features/settings/presentation/widgets/sliding_switch_widget.dart";
 
 import "../../../core/themes/colors.dart";
 
@@ -14,72 +17,69 @@ class SettingsScreen extends StatelessWidget {
         title: Text("Settings", style: AppTextStyles.pageTitle),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          crossAxisAlignment: .start,
-          children: [
-            Container(
-              height: 30,
-              width: double.infinity,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(16),
-              //color: AppColors.primary.withOpacity(0.1),
-            ),
-            child: Switch(
-              value: false,
-              onChanged: (value) {},
-              activeColor: AppColors.primary.withOpacity(0.1),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: .start,
+            children: [
+              Text("Units of measurement", style: AppTextStyles.sectionHeader),
+              smallSpaceSize,
+              Container(
+                height: 45,
+                width: double.infinity,
+                padding: const EdgeInsets.all(4.0),
+                decoration: BoxDecoration(
+                  color: AppColors.bone,
+                  borderRadius: BorderRadius.circular(18),
+                  border: Border.all(color: AppColors.charcoal.withOpacity(0.1)),
+                ),
+                child: Row(
+                  children: [
+                    SlidingSwitchWidget(
+                      label: "Metric (g,ml)",
+                      isSelected: true,
+                      onTap: () {},
+                    ),
+                    smallSpaceSize,
+                    SlidingSwitchWidget(
+                      label: "Imperial (oz, lb)",
+                      isSelected: false,
+                      onTap: () {},
+                    ),
+                  ],
+                ),
+              ),
+              mediumSpaceSize,
+              Text("Dietary Restrictions", style: AppTextStyles.sectionHeader),
+              smallSpaceSize,
+              ItemCheckBoxWidget(title: "Vegetarian"),
+              Divider(thickness: 0.1),
+              ItemCheckBoxWidget(title: "Vegan"),
+              Divider(thickness: 0.1),
+              ItemCheckBoxWidget(title: "Gluten-Free"),
+              Divider(thickness: 0.1),
+              ItemCheckBoxWidget(title: "Dairy-Free"),
+              Divider(thickness: 0.1),
+              mediumSpaceSize,
+              Text("App Theme", style: AppTextStyles.sectionHeader),
+              smallSpaceSize,
+              ItemSwitchWidget(
+                title: "Light Mode",
+                icon: Icon(Icons.light_mode_outlined),
+              ),
+              mediumSpaceSize,
+              Text("Notifications", style: AppTextStyles.sectionHeader),
+              smallSpaceSize,
+              ItemSwitchWidget(title: "Push Notifications", value: true),
+              Divider(thickness: 0.1),
+              ItemSwitchWidget(title: "Recipe Recommendations", value: true),
+              mediumSpaceSize,
+              Text("Support", style: AppTextStyles.sectionHeader),
+              smallSpaceSize,
               
-            ),
-
-            ),
-            // Text("Preferences", style: AppTextStyles.sectionHeader),
-            // mediumSpaceSize,
-            // ListTile(
-            //   title: Text("Units of Measurements"),
-            //   subtitle: Text("Metrics"),
-            //   trailing: Switch(
-            //     value: false,
-            //     onChanged: (value) {},
-            //     activeColor: AppColors.primary,
-            //   ),
-            // ),
-            // smallSpaceSize,
-            // ListTile(
-            //   title: Text("Dietary Restrictions"),
-            //   trailing: Icon(Icons.chevron_right),
-            // ),
-            // smallSpaceSize,
-            // ListTile(
-            //   title: Text("App Theme"),
-            //   subtitle: Text("Light"),
-            //   trailing: Switch(
-            //     value: false,
-            //     onChanged: (value) {},
-            //     activeColor: AppColors.primary,
-            //   ),
-            // ),
-            // smallSpaceSize,
-            // ListTile(
-            //   title: Text("Notifications"),
-            //   subtitle: Text("Off"),
-            //   trailing: Switch(
-            //     value: false,
-            //     onChanged: (value) {},
-            //     activeColor: AppColors.primary,
-            //   ),
-            // ),
-            // mediumSpaceSize,
-            // Text("Support", style: AppTextStyles.sectionHeader),
-            // smallSpaceSize,
-            // ListTile(
-            //   title: Text("Help & Support"),
-            //   trailing: Icon(Icons.chevron_right),
-            // ),
-            // smallSpaceSize,
-            // ListTile(title: Text("About"), trailing: Icon(Icons.chevron_right)),
-          ],
+            ],
+          ),
         ),
       ),
     );
