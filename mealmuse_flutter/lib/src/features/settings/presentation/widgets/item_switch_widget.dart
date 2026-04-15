04f8d9097f863cf805572e582a9995f5028068c9
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2026-04-15 17:44:04 +0300

    Design of the item switch button

---
+++ mealmuse_flutter/lib/src/features/settings/presentation/widgets/item_switch_widget.dart
@@ -0,0 +1,35 @@
+import "package:flutter/material.dart";
+
+import "../../../../core/themes/colors.dart";
+import "../../../../core/themes/text_styles.dart";
+
+class ItemSwitchWidget extends StatelessWidget {
+  final String title;
+  final Icon? icon;
+  final bool value;
+
+  const ItemSwitchWidget({
+    super.key,
+    required this.title,
+    this.icon,
+    this.value = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SwitchListTile(
+      title: Text(title, style: AppTextStyles.bodyText),
+      value: value,
+      onChanged: (newValue) {
+        // Handle slider change
+      },
+      controlAffinity: ListTileControlAffinity.trailing,
+      secondary: icon,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0)),
+      hoverColor: AppColors.mutedText.withOpacity(0.1),
+      //thumbColor: WidgetStatePropertyAll(AppColors.mutedText.withOpacity(0.5)),
+      //trackColor: WidgetStatePropertyAll(AppColors.mutedText.withOpacity(0.1)),
+      trackOutlineColor: WidgetStatePropertyAll(AppColors.charcoal.withOpacity(0.2)),
+    );
+  }
+}
