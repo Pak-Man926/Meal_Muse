e241a11f931e733dda5d611800ea94a3b23944f1
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2026-04-15 17:43:48 +0300

    Design of the sliding switch widget

---
+++ mealmuse_flutter/lib/src/features/settings/presentation/widgets/sliding_switch_widget.dart
@@ -0,0 +1,52 @@
+import "package:flutter/material.dart";
+import "package:meal_muse/src/core/themes/colors.dart";
+
+import "../../../../core/themes/text_styles.dart";
+
+class SlidingSwitchWidget extends StatelessWidget {
+  final String label;
+  final bool isSelected;
+  final Function()? onTap;
+
+  const SlidingSwitchWidget({
+    super.key,
+    required this.label,
+    this.isSelected = true,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: onTap,
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 200),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: isSelected ? Colors.white : Colors.transparent,
+            borderRadius: BorderRadius.circular(18),
+            boxShadow: isSelected
+                ? [
+                    BoxShadow(
+                      color: AppColors.charcoal.withOpacity(0.1),
+                      blurRadius: 4,
+                      offset: const Offset(0, 0),
+                    ),
+                  ]
+                : [],
+          ),
+          child: Text(
+            label,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: AppTextStyles.bodyText.copyWith(
+              color: isSelected ? AppColors.charcoal : AppColors.mutedText,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
