435aa261b05a0f48750a094cd8f367fb7d30ce85
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2026-04-21 09:04:23 +0300

    Design of the trending recipe screen

---
+++ mealmuse_flutter/lib/src/features/recipes/presentation/trending_recipes_list_screen.dart
@@ -0,0 +1,115 @@
+import "package:flutter/material.dart";
+import "package:meal_muse/src/core/constants/constants.dart";
+import "package:meal_muse/src/features/saved/domain/recipe_model.dart";
+import "package:meal_muse/src/features/saved/presentation/widgets/saved_item_widget.dart";
+import "package:meal_muse/src/features/schedule/presentation/widgets/schedule_meal_card_widget.dart";
+import "package:meal_muse/src/features/search/presentation/widgets/container_widget.dart";
+
+import "../../../core/themes/colors.dart";
+import "../../../core/themes/text_styles.dart";
+
+final List<Recipe> mySavedMeals = [
+  Recipe(
+    mealType: "Breakfast",
+    meal: "Blueberry Pancakes",
+    prepTime: 20,
+    composition: 580,
+    imageAddress: "assets/Fluffy-blueberry-pancakes-1.jpg",
+  ),
+  Recipe(
+    mealType: "Breakfast",
+    meal: "Avocado Toast",
+    prepTime: 10,
+    composition: 320,
+    imageAddress: "assets/Avocado-Toast-SpendWithPennies-1.jpg",
+  ),
+  Recipe(
+    mealType: "Breakfast",
+    meal: "Eggs Benedict",
+    prepTime: 35,
+    composition: 450,
+    imageAddress:
+        "assets/17205-eggs-benedict-DDMFS-4x3-a0042d5ae1da485fac3f468654187db0.jpg",
+  ),
+  Recipe(
+    mealType: "Breakfast",
+    meal: "Berry Smoothie Bowl",
+    prepTime: 15,
+    composition: 45,
+    imageAddress: "assets/Protein-Berry-Smoothie-Bowl-1.jpg",
+  ),
+];
+
+class TrendingRecipesListScreen extends StatelessWidget {
+  const TrendingRecipesListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Trending Recipes",
+          style: AppTextStyles.pageTitle.copyWith(color: AppColors.primary),
+        ),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          mainAxisAlignment: .start,
+          crossAxisAlignment: .start,
+          children: [
+            // Text(
+            //   "Breakfast Recipes",
+            //   style: AppTextStyles.sectionHeader.copyWith(
+            //     fontSize: 26,
+            //     fontWeight: FontWeight.bold,
+            //   ),
+            // ),
+            // smallSpaceSize,
+            // Text(
+            //   "Start your day right with our curated collection of delicious and energizing breakfast recipes. From quick and easy options to hearty and indulgent meals, we have something for everyone.",
+            //   style: AppTextStyles.bodyText,
+            //   overflow: TextOverflow.clip,
+            //   maxLines: 5,
+            // ),
+            smallSpaceSize,
+            Container(
+              height: 30,
+              width: double.infinity,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  ContainerWidget.extended(label: "All Recipes"),
+                  const SizedBox(width: 10),
+                  ContainerWidget.extended(label: "Quick & Easy"),
+                  const SizedBox(width: 10),
+                  ContainerWidget.extended(label: "Healthy"),
+                  const SizedBox(width: 10),
+                  ContainerWidget.extended(label: "Quick & Easy"),
+                  const SizedBox(width: 10),
+                  ContainerWidget.extended(label: "Healthy"),
+                ],
+              ),
+            ),
+            smallSpaceSize,
+            Expanded(
+              child: ListView.builder(
+                itemCount: mySavedMeals.length,
+                itemBuilder: (context, index) {
+                  return ScheduleCardWidget(
+                    mealType: mySavedMeals[index].mealType!,
+                    meal: mySavedMeals[index].meal,
+                    prepTime: mySavedMeals[index].prepTime,
+                    composition: mySavedMeals[index].composition,
+                    imageAddress: mySavedMeals[index].imageAddress,
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
