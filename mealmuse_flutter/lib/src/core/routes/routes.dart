5e20b764002921657884a19871d23e2bbc9029f9
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2026-04-21 09:03:52 +0300

    Definition of routes to the designed recipe pages

--- mealmuse_flutter/lib/src/core/routes/routes.dart
+++ mealmuse_flutter/lib/src/core/routes/routes.dart
@@ -1,9 +1,15 @@
 import "package:go_router/go_router.dart";
 import "package:meal_muse/src/features/dashboard/presentation/dashboard_screen.dart";
+import "package:meal_muse/src/features/recipes/presentation/baked_recipe_list_screen.dart";
 import "package:meal_muse/src/features/recipes/presentation/breakfast_recipe_list_screen.dart";
+import "package:meal_muse/src/features/recipes/presentation/deserts_recipe_list_screen.dart";
 import "package:meal_muse/src/features/recipes/presentation/dinner_recipe_list_screen.dart";
+import "package:meal_muse/src/features/recipes/presentation/drinks_recipe_list_screen.dart";
 import "package:meal_muse/src/features/recipes/presentation/lunch_recipe_list_screen.dart";
 import "package:meal_muse/src/features/recipes/presentation/recipe_detail_screen.dart";
+import "package:meal_muse/src/features/recipes/presentation/snacks_recipe_list_screen.dart";
+import "package:meal_muse/src/features/recipes/presentation/soups_recipe_list_screen.dart";
+import "package:meal_muse/src/features/recipes/presentation/trending_recipes_list_screen.dart";
 import "package:meal_muse/src/features/settings/presentation/settings_screen.dart";
 
 final router = GoRouter(
@@ -26,6 +32,30 @@ final router = GoRouter(
     GoRoute(
       path: "/dinnerrecipes",
       builder: (context, state) => DinnerRecipeListScreen(),
+    ),
+    GoRoute(
+      path: "/drinkrecipes",
+      builder: (context, state) => DrinksRecipeListScreen(),
+    ),
+    GoRoute(
+      path: "/desertrecipes",
+      builder: (context, state) => DesertRecipeListScreen(),
+    ),
+    GoRoute(
+      path: "/souprecipes",
+      builder: (context, state) => SoupRecipeListScreen(),
+    ),
+    GoRoute(
+      path: "/snackrecipes",
+      builder: (context, state) => SnacksRecipeListScreen(),
+    ),
+    GoRoute(
+      path: "/bakedrecipes",
+      builder: (context, state) => BakedRecipeListScreen(),
+    ),
+    GoRoute(
+      path: "/trendingrecipes",
+      builder: (context, state) => TrendingRecipesListScreen(),
     ),
   ],
 );
