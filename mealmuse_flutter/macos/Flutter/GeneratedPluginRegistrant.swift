9913f7991f900cbb6a9763afe10b2bebd17339a0
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2026-06-05 17:13:22 +0300

    Device UUID additions

--- mealmuse_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mealmuse_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,12 @@ import Foundation
 
 import connectivity_plus
 import package_info_plus
+import platform_device_id
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PlatformDeviceIdMacosPlugin.register(with: registry.registrar(forPlugin: "PlatformDeviceIdMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
