97ddba1ce63f28704627644a426de3329037016e
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2026-06-15 23:57:51 +0300

    Gradle error fix and removal of discontinued package

--- mealmuse_flutter/android/build.gradle.kts
+++ mealmuse_flutter/android/build.gradle.kts
@@ -12,6 +12,22 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
+// 1. We put the namespace patch HERE, before evaluationDependsOn
+subprojects {
+    afterEvaluate {
+        // Only apply this to Android libraries (like the outdated device_info package)
+        if (project.plugins.hasPlugin("com.android.library")) {
+            project.configure<com.android.build.gradle.LibraryExtension> {
+                if (namespace == null) {
+                    namespace = project.group.toString()
+                }
+            }
+        }
+    }
+}
+
+// 2. Flutter's evaluation rule must come AFTER the afterEvaluate block
 subprojects {
     project.evaluationDependsOn(":app")
 }
