c035022f969d332d63f2dda34272aa7e1d48851b
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2026-06-16 12:11:00 +0300

    Definition of the auth call everytime the app starts

--- mealmuse_flutter/lib/main.dart
+++ mealmuse_flutter/lib/main.dart
@@ -1,13 +1,37 @@
 import "package:flutter/material.dart";
 import "package:flutter_dotenv/flutter_dotenv.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:get_storage/get_storage.dart";
+import "package:logger/logger.dart";
 import "package:meal_muse/src/app.dart";
 import "package:package_info_plus/package_info_plus.dart";
 import "package:meal_muse/src/core/constants/constants.dart";
+import "package:meal_muse/src/features/auth/data/register_user_repository.dart";
 
+final logger = Logger();
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   packageInfo = await PackageInfo.fromPlatform();
+  await GetStorage.init();
   await dotenv.load();
+
+  // Soft Auth: Register device UUID in the background silently
+  try {
+    RegisterUserRepository()
+        .registerUser()
+        .then((registerUser) {
+          logger.i(
+            "Soft Auth: User registered with ID: ${registerUser.userId}",
+          );
+        })
+        .catchError((error) {
+          logger.e("Soft Auth: Failed to register user. Error: $error");
+        });
+  } catch (e, stackTrace) {
+    logger.e(
+      "Soft Auth: Exception occurred during user registration. Error: $e StackTrace: $stackTrace",
+    );
+  }
+
   runApp(ProviderScope(child: const MyApp()));
 }
